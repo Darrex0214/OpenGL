c1e975f552994140104530d0ad4fc8142e41f9a0
Jose <danielramirezdelgado32@gmail.com>
2023-09-26 10:51:01 -0500

    NEw

--- main.cpp
+++ main.cpp
@@ -2,6 +2,7 @@
 // Sample Application: Lighting (Per Fragment Phong)
 //=============================================================================
 
+
 #include <GL/glew.h>
 #include <GL/freeglut.h>
 #include "glApplication.h"
@@ -52,7 +53,6 @@ public:
 
 		glGenTextures(1, &texid);
 		glBindTexture(GL_TEXTURE_2D, texid);
-		glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_SRGB_DECODE_EXT, GL_TRUE);
 		glTexEnvi(GL_TEXTURE_2D, GL_TEXTURE_ENV_MODE, GL_MODULATE);
 		glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST);
 		glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST);
@@ -426,67 +426,70 @@ public:
    
    void DemoLight(void)
    {
-     glEnable(GL_LIGHTING);
-     glEnable(GL_LIGHT0);
-     glEnable(GL_NORMALIZE);
-     
-     // Light model parameters:
-     // -------------------------------------------
-     
-     GLfloat lmKa[] = {0.0, 0.0, 0.0, 0.0 };
-     glLightModelfv(GL_LIGHT_MODEL_AMBIENT, lmKa);
-     
-     glLightModelf(GL_LIGHT_MODEL_LOCAL_VIEWER, 1.0);
-     glLightModelf(GL_LIGHT_MODEL_TWO_SIDE, 0.0);
-     
-     // -------------------------------------------
-     // Spotlight Attenuation
-     
-     GLfloat spot_direction[] = {1.0, -1.0, -1.0 };
-     GLint spot_exponent = 30;
-     GLint spot_cutoff = 180;
-     
-     glLightfv(GL_LIGHT0, GL_SPOT_DIRECTION, spot_direction);
-     glLighti(GL_LIGHT0, GL_SPOT_EXPONENT, spot_exponent);
-     glLighti(GL_LIGHT0, GL_SPOT_CUTOFF, spot_cutoff);
-    
-     GLfloat Kc = 1.0;
-     GLfloat Kl = 0.0;
-     GLfloat Kq = 0.0;
-     
-     glLightf(GL_LIGHT0, GL_CONSTANT_ATTENUATION,Kc);
-     glLightf(GL_LIGHT0, GL_LINEAR_ATTENUATION, Kl);
-     glLightf(GL_LIGHT0, GL_QUADRATIC_ATTENUATION, Kq);
-     
-     
-     // ------------------------------------------- 
-     // Lighting parameters:
+	   glEnable(GL_LIGHTING);
+	   glEnable(GL_LIGHT0);
+	   glEnable(GL_NORMALIZE);
 
-     GLfloat light_pos[] = {0.0f, 5.0f, 5.0f, 1.0f};
-     GLfloat light_Ka[]  = {1.0f, 0.5f, 0.5f, 1.0f};
-     GLfloat light_Kd[]  = {1.0f, 0.1f, 0.1f, 1.0f};
-     GLfloat light_Ks[]  = {1.0f, 1.0f, 1.0f, 1.0f};
+	   // Light model parameters:
+	   // -------------------------------------------
 
-     glLightfv(GL_LIGHT0, GL_POSITION, light_pos);
-     glLightfv(GL_LIGHT0, GL_AMBIENT, light_Ka);
-     glLightfv(GL_LIGHT0, GL_DIFFUSE, light_Kd);
-     glLightfv(GL_LIGHT0, GL_SPECULAR, light_Ks);
+	   GLfloat lmKa[] = { 4.0f, 4.0f, 4.0f, 1.0f }; // Luz ambiental más brillante
+	   glLightModelfv(GL_LIGHT_MODEL_AMBIENT, lmKa);
 
-     // -------------------------------------------
-     // Material parameters:
+	   glLightModelf(GL_LIGHT_MODEL_LOCAL_VIEWER, 1.0);
+	   glLightModelf(GL_LIGHT_MODEL_TWO_SIDE, 0.0);
 
-     GLfloat material_Ka[] = {0.5f, 0.0f, 0.0f, 1.0f};
-     GLfloat material_Kd[] = {0.4f, 0.4f, 0.5f, 1.0f};
-     GLfloat material_Ks[] = {0.8f, 0.8f, 0.0f, 1.0f};
-     GLfloat material_Ke[] = {0.1f, 0.0f, 0.0f, 0.0f};
-     GLfloat material_Se = 20.0f;
+	   // -------------------------------------------
+	   // Spotlight Attenuation
 
-     glMaterialfv(GL_FRONT_AND_BACK, GL_AMBIENT, material_Ka);
-     glMaterialfv(GL_FRONT_AND_BACK, GL_DIFFUSE, material_Kd);
-     glMaterialfv(GL_FRONT_AND_BACK, GL_SPECULAR, material_Ks);
-     glMaterialfv(GL_FRONT_AND_BACK, GL_EMISSION, material_Ke);
-     glMaterialf(GL_FRONT_AND_BACK, GL_SHININESS, material_Se);
+	   GLfloat spot_direction[] = { 1.0, -1.0, -1.0 };
+	   GLint spot_exponent = 60; // Mayor exponente
+	   GLint spot_cutoff = 90;  // Mayor ángulo de corte
+
+	   glLightfv(GL_LIGHT0, GL_SPOT_DIRECTION, spot_direction);
+	   glLighti(GL_LIGHT0, GL_SPOT_EXPONENT, spot_exponent);
+	   glLighti(GL_LIGHT0, GL_SPOT_CUTOFF, spot_cutoff);
+
+	   GLfloat Kc = 0.5;  // Atenuación constante moderada
+	   GLfloat Kl = 0.01; // Atenuación lineal reducida
+	   GLfloat Kq = 0.001; // Atenuación cuadrática reducida
+
+	   glLightf(GL_LIGHT0, GL_CONSTANT_ATTENUATION, Kc);
+	   glLightf(GL_LIGHT0, GL_LINEAR_ATTENUATION, Kl);
+	   glLightf(GL_LIGHT0, GL_QUADRATIC_ATTENUATION, Kq);
+
+	   // -------------------------------------------
+	   // Lighting parameters:
+
+	   GLfloat light_pos[] = { 0.0f, 5.0f, 5.0f, 1.0f };
+	   GLfloat light_Ka[] = { 0.8f, 0.8f, 0.8f, 1.0f }; // Intensidad ambiental moderada
+	   GLfloat light_Kd[] = { 1.0f, 1.0f, 1.0f, 1.0f }; // Intensidad difusa máxima
+	   GLfloat light_Ks[] = { 1.0f, 1.0f, 1.0f, 1.0f }; // Intensidad especular máxima
+
+	   glLightfv(GL_LIGHT0, GL_POSITION, light_pos);
+	   glLightfv(GL_LIGHT0, GL_AMBIENT, light_Ka);
+	   glLightfv(GL_LIGHT0, GL_DIFFUSE, light_Kd);
+	   glLightfv(GL_LIGHT0, GL_SPECULAR, light_Ks);
+
+	   // -------------------------------------------
+	   // Material parameters:
+
+	   GLfloat material_Ka[] = { 0.8f, 0.8f, 0.8f, 1.0f };
+	   GLfloat material_Kd[] = { 0.8f, 0.8f, 0.8f, 1.0f };
+	   GLfloat material_Ks[] = { 1.0f, 1.0f, 1.0f, 1.0f }; // Intensidad especular máxima
+	   GLfloat material_Ke[] = { 0.0f, 0.0f, 0.0f, 0.0f };
+	   GLfloat material_Se = 50.0f; // Aumenta el exponente de brillo (shininess) para una especular más intensa
+
+	   glMaterialfv(GL_FRONT_AND_BACK, GL_AMBIENT, material_Ka);
+	   glMaterialfv(GL_FRONT_AND_BACK, GL_DIFFUSE, material_Kd);
+	   glMaterialfv(GL_FRONT_AND_BACK, GL_SPECULAR, material_Ks);
+	   glMaterialfv(GL_FRONT_AND_BACK, GL_EMISSION, material_Ke);
+	   glMaterialf(GL_FRONT_AND_BACK, GL_SHININESS, material_Se);
    }
+
+
+
+
 };
 
 //-----------------------------------------------------------------------------
