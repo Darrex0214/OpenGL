f0655183427aa4e4570375c7c689e59a4aa97b07
Jose <danielramirezdelgado32@gmail.com>
2023-10-01 13:35:25 -0500

    Se corrigen los archivos vectershader y fragmentshader para corregir la representacion de colores

--- main.cpp
+++ main.cpp
@@ -108,21 +108,6 @@ public:
 
 
 		  glPushMatrix();
-
-		  glColor3f(0.0f, 1.0f, 0.0f);
-		  GLfloat material_Ka[] = { 0.0f, 0.5f, 0.0f, 1.0f };
-		  GLfloat material_Kd[] = { 0.0f, 1.0f, 0.0f, 1.0f };
-		  GLfloat material_Ks[] = { 0.0f, 1.0f, 0.0f, 1.0f };
-		  GLfloat material_Ke[] = { 0.1f, 0.0f, 0.0f, 0.0f };
-		  GLfloat material_Se = 128.0f;
-
-		  glMaterialfv(GL_FRONT_AND_BACK, GL_AMBIENT, material_Ka);
-		  glMaterialfv(GL_FRONT_AND_BACK, GL_DIFFUSE, material_Kd);
-		  glMaterialfv(GL_FRONT_AND_BACK, GL_SPECULAR, material_Ks);
-		  glMaterialfv(GL_FRONT_AND_BACK, GL_EMISSION, material_Ke);
-		  glMaterialf(GL_FRONT_AND_BACK, GL_SHININESS, material_Se);
-
-
 		  glTranslatef(0.0f, -1.5f, 0.0f);
 		  glScalef(3, 0.001, 3);
 		  glutSolidCube(10);
@@ -361,68 +346,49 @@ public:
    }
 
    
-   void DemoLight(void)
+   void DemoLight()
    {
+	   // Habilitar iluminación y luces
 	   glEnable(GL_LIGHTING);
 	   glEnable(GL_LIGHT0);
-	   glEnable(GL_NORMALIZE);
 
-	   // Light model parameters:
-	   // -------------------------------------------
+	   // Posición de la luz
+	   GLfloat light_position[] = { 1.0f, 1.0f, 1.0f, 0.0f };
+	   glLightfv(GL_LIGHT0, GL_POSITION, light_position);
 
-	   GLfloat lmKa[] = { 4.0f, 4.0f, 4.0f, 1.0f }; // Luz ambiental más brillante
-	   glLightModelfv(GL_LIGHT_MODEL_AMBIENT, lmKa);
+	   // Dibujar objetos con diferentes colores y materiales
+	   glPushMatrix();
 
-	   glLightModelf(GL_LIGHT_MODEL_LOCAL_VIEWER, 1.0);
-	   glLightModelf(GL_LIGHT_MODEL_TWO_SIDE, 0.0);
+	   // Material rojo
+	   GLfloat material_Ka[] = { 0.0f, 0.0f, 0.0f, 1.0f };
+	   GLfloat material_Kd[] = { 1.0f, 0.0f, 0.0f, 1.0f };
+	   GLfloat material_Se = 128.0f;
+	   glMaterialfv(GL_FRONT, GL_AMBIENT, material_Ka);
+	   glMaterialfv(GL_FRONT, GL_DIFFUSE, material_Kd);
+	   glMaterialf(GL_FRONT, GL_SHININESS, material_Se);
+	   glTranslatef(-1.5f, 0.0f, 0.0f);
+	   glutSolidSphere(1.0, 20, 20);
 
-	   // -------------------------------------------
-	   // Spotlight Attenuation
+	   // Material verde
+	   GLfloat material_Kd_green[] = { 0.0f, 1.0f, 0.0f, 1.0f };
+	   glMaterialfv(GL_FRONT, GL_DIFFUSE, material_Kd_green);
+	   glTranslatef(3.0f, 0.0f, 0.0f);
+	   glutSolidSphere(1.0, 20, 20);
 
-	   GLfloat spot_direction[] = { 1.0, -1.0, -1.0 };
-	   GLint spot_exponent = 60; // Mayor exponente
-	   GLint spot_cutoff = 90;  // Mayor ángulo de corte
+	   // Material azul
+	   GLfloat material_Kd_blue[] = { 0.0f, 0.0f, 1.0f, 1.0f };
+	   glMaterialfv(GL_FRONT, GL_DIFFUSE, material_Kd_blue);
+	   glTranslatef(-1.5f, -3.0f, 0.0f);
+	   glutSolidSphere(1.0, 20, 20);
 
-	   glLightfv(GL_LIGHT0, GL_SPOT_DIRECTION, spot_direction);
-	   glLighti(GL_LIGHT0, GL_SPOT_EXPONENT, spot_exponent);
-	   glLighti(GL_LIGHT0, GL_SPOT_CUTOFF, spot_cutoff);
+	   glPopMatrix();
 
-	   GLfloat Kc = 0.5;  // Atenuación constante moderada
-	   GLfloat Kl = 0.01; // Atenuación lineal reducida
-	   GLfloat Kq = 0.001; // Atenuación cuadrática reducida
-
-	   glLightf(GL_LIGHT0, GL_CONSTANT_ATTENUATION, Kc);
-	   glLightf(GL_LIGHT0, GL_LINEAR_ATTENUATION, Kl);
-	   glLightf(GL_LIGHT0, GL_QUADRATIC_ATTENUATION, Kq);
-
-	   // -------------------------------------------
-	   // Lighting parameters:
-
-	   GLfloat light_pos[] = { 0.0f, 5.0f, 5.0f, 1.0f };
-	   GLfloat light_Ka[] = { 0.8f, 0.8f, 0.8f, 1.0f }; // Intensidad ambiental moderada
-	   GLfloat light_Kd[] = { 1.0f, 1.0f, 1.0f, 1.0f }; // Intensidad difusa máxima
-	   GLfloat light_Ks[] = { 1.0f, 1.0f, 1.0f, 1.0f }; // Intensidad especular máxima
-
-	   glLightfv(GL_LIGHT0, GL_POSITION, light_pos);
-	   glLightfv(GL_LIGHT0, GL_AMBIENT, light_Ka);
-	   glLightfv(GL_LIGHT0, GL_DIFFUSE, light_Kd);
-	   glLightfv(GL_LIGHT0, GL_SPECULAR, light_Ks);
-
-	   // -------------------------------------------
-	   // Material parameters:
-
-	   GLfloat material_Ka[] = { 0.8f, 0.8f, 0.8f, 1.0f };
-	   GLfloat material_Kd[] = { 0.8f, 0.8f, 0.8f, 1.0f };
-	   GLfloat material_Ks[] = { 1.0f, 1.0f, 1.0f, 1.0f }; // Intensidad especular máxima
-	   GLfloat material_Ke[] = { 0.0f, 0.0f, 0.0f, 0.0f };
-	   GLfloat material_Se = 50.0f; // Aumenta el exponente de brillo (shininess) para una especular más intensa
-
-	   glMaterialfv(GL_FRONT_AND_BACK, GL_AMBIENT, material_Ka);
-	   glMaterialfv(GL_FRONT_AND_BACK, GL_DIFFUSE, material_Kd);
-	   glMaterialfv(GL_FRONT_AND_BACK, GL_SPECULAR, material_Ks);
-	   glMaterialfv(GL_FRONT_AND_BACK, GL_EMISSION, material_Ke);
-	   glMaterialf(GL_FRONT_AND_BACK, GL_SHININESS, material_Se);
+	   // Deshabilitar iluminación
+	   glDisable(GL_LIGHTING);
+	   glDisable(GL_LIGHT0);
    }
+
+
 
 
 
